c49caff9f428ec06973437bf92b1cd2373021920
Arlin Schaffel <arlin@fexd.com>
2021-05-04 19:55:01 -0600

    Retweet SKGov & SaskHealth
    * check for new tweets every minute (same run loop as checking website)
    * use tweepy to check for new tweets from SKGov & SaskHealth, and try to determine if they are about eligible age limits
    * wording appears to change regularly, so we may miss a few to start - considering changing to just look for "eligible' or 'eligibility' in future
    * Even tho once a minute seems like a lot, limit per app auth are in the hundreds per 15 minutes

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from dotenv import load_dotenv
 
 
 def update_status(status):
-    load_dotenv()
     auth = tweepy.OAuthHandler(os.getenv('CONSUMER_KEY'), os.getenv('CONSUMER_SECRET'))
     auth.set_access_token(os.getenv('ACCESS_TOKEN'), os.getenv('ACCESS_TOKEN_SECRET'))
 
@@ -17,8 +16,8 @@ def update_status(status):
         api = tweepy.API(auth)
         api.update_status(status)
         log('Tweet:', status)
-    except tweepy.TweepError:
-        log('Error: update_status: Something went wrong!')
+    except tweepy.TweepError as error:
+        log('Error: update_status: Something went wrong!', error)
 
 
 def get_html(url):
@@ -76,6 +75,24 @@ def set_previous(data, file_name='previous.json'):
     file.close()
     log(f'{file_name} updated: {write}')
 
+def get_last_tweet_id(account_name):
+    # Default tweet_id of SaskHealth tweet from May 5, 2021
+    read_json = '1390055318491582465'
+    file_name = f'{account_name}.txt'
+    if os.path.exists(file_name):
+        file = open(file_name, 'r')
+        read_json = json.load(file)
+        file.close()
+    return int(read_json)
+
+
+def set_last_tweet_id(account_name, tweet_id):
+    file_name = f'{account_name}.txt'
+    file = open(file_name, 'w')
+    file.write(str(tweet_id))
+    file.close()
+    log(f'{file_name} updated: {str(tweet_id)}')
+
 
 def should_tweet(booking_info, previous, current_time):
     # check if there is a change and if we have already tweeted today
@@ -103,8 +120,54 @@ def log(text, more_text=''):
     print(f"{datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')}: {text} {more_text}")
 
 
+def check_tweets():
+    auth = tweepy.OAuthHandler(os.getenv('CONSUMER_KEY'), os.getenv('CONSUMER_SECRET'))
+    auth.set_access_token(os.getenv('ACCESS_TOKEN'), os.getenv('ACCESS_TOKEN_SECRET'))
+
+    accounts = [
+        {'name': 'SaskHealth', 'id': 134198093},
+        {'name': 'SKGov', 'id': 281756281}
+    ]
+
+    for account in accounts:
+        try:
+            api = tweepy.API(auth)
+            since_id = get_last_tweet_id(account['name'])
+            timeline = api.user_timeline(user_id=account['id'], since_id=int(since_id), include_rts=False, exclude_replies=True, trim_user=True, tweet_mode='extended')
+            tweet_ids = []
+            for t in timeline:
+                tweet_ids.append(t.id)
+                if should_retweet(t.full_text):
+                    api.retweet(t.id)
+                    log('Retweet:', t)
+            tweet_ids.sort(reverse=True)
+            if len(tweet_ids) > 0:
+                set_last_tweet_id(account['name'], tweet_ids[0])
+
+        except tweepy.TweepError as error:
+            log('Error: dev_checktweets: Something went wrong!', error)
+
+
+def should_retweet(tweet_text):
+    log('should_retweet: should we retweet?')
+    eligibility_strings = [
+        'are now eligible to book',
+        'eligibility is now',
+        'immunization age eligibility',
+        'eligibility for immunization',
+        'eligibility in the provincial immunization program',
+    ]
+    for eligibility_string in eligibility_strings:
+        if tweet_text.find(eligibility_string) >= 0:
+            log('should_retweet: matches found, we should retweet', tweet_text)
+            return True
+    log('should_retweet: no matching strings found', tweet_text)
+    return False
+
+
 if __name__ == '__main__':
     log('Sask Vaccine Bot is now running...')
+    load_dotenv()
     while True:
         log('Checking for updates...')
         vaccine_site = "https://www.saskatchewan.ca/government/health-care-administration-and-provider-resources/treatment-procedures-and-guidelines/emerging-public-health-issues/2019-novel-coronavirus/covid-19-vaccine/vaccine-booking#check-your-eligibility"
@@ -125,5 +188,5 @@ if __name__ == '__main__':
                 log('Error: main: current_booking is None')
         else:
             log('Error: main: html is None')
-
+        check_tweets()
         time.sleep(random.randint(60, 120))  # wait 1 minute
--- test_main.py
+++ test_main.py
@@ -118,10 +118,33 @@ def test_should_tweet():
         assert result == i['expect'], f'should_tweet() failed, {i["id"]}'
 
 
+def test_should_retweet():
+    inputs = [
+        {'expect': False, 'tweet': ''},
+        {'expect': False, 'tweet': '⚠️PSA: Regina adjusts Level 3 visitor restrictions in long-term care to allow limited outdoor visitation.\n\nOutdoor visits in Regina, Emerald Park, Lumsden &amp; Cupar care homes can occur w/up to 2 support people from the same household at one time.\n\nMore: https://t.co/LK335Oen8C https://t.co/p7NUGhyeb3'},
+        {'expect': False, 'tweet': "Don't take the chance of passing COVID-19 on. Stay home if ill even if symptoms mild &amp; don't wait to get tested. COVID-19 spreads easily.\n\nFollow public health orders &amp; public health advice. Call 911 in emergencies or HealthLine 811 for advice and/or to arrange COVID-19 testing. https://t.co/g8OiSubAny"},
+        {'expect': True, 'tweet': 'COVID-19 immunization age eligibility for May 5 remains at 37+ provincially &amp; 18+ for those living in the NSAD. Reminder to residents that clinic availability is based on vaccine availability, and demand for appointments outweighs the current supply. More: https://t.co/oVigqgV0sB https://t.co/yuaCUWhRrX'},
+        {'expect': True, 'tweet': 'Happy #StarWarsDay2021. #COVID19sk vaccine eligibility is now 37 yrs+ provincially &amp; 18 yrs+ in the NSAD. Are you wearing your best Star Wars apparel to your COVID-19 vaccination appointment/drive-thru site today to celebrate? Tag us with your photos! #MayThe4thBeWithYou https://t.co/Kf5e4VCM41'},
+        {'expect': True, 'tweet': 'Saskatchewan residents 40+ and remaining prioritized front-line workers are now eligible to book their #COVID19SK vaccination. \n\nView a list of vaccination pilot pharmacies at https://t.co/ZM7z12jaHR. https://t.co/ZjfcWCVN5O'},
+        {'expect': False, 'tweet': '#COVID19SK Update for April 30: 427,739 Vaccines Administered, 264 New Cases, 252 Recoveries, 173 in Hospital, Four New Deaths\n\nCurrent 7-Day Average: 241 (19.7 per 100,000).\n\nFull details:  https://t.co/hkLRB0Taa0\n\nCOVID-19 Dashboard: https://t.co/kvBuGtZGWW https://t.co/TFieqT8mBP'},
+        {'expect': False, 'tweet': 'One of the most common angling infractions in #SK is improper packaging when transporting fish. Understand the regulations, such as proper packaging, by reviewing the 2021-22 Anglers Guide on https://t.co/xo7kt3o1FY. https://t.co/P6yf0JH5zB'},
+        {'expect': True, 'tweet': 'Saskatchewan residents 37+ are now eligible to book their #COVID19SK vaccination. \n\nBook online: https://t.co/OK1ksAAjQc\n\nOr by phone: 1-833-SASK-VAX (1-833-727-5829) https://t.co/Fr9BO3jxaf'},
+        {'expect': True, 'tweet': '⚠️Effective 8 am May 6 eligibility in the provincial immunization program moves to 35+, except for residents living in the NSAD where it remains 18+. Applies to all immunization clinics: booked appointments, pharmacies, drive-thru/walk-in & mobile.\nMore: https://saskhealthauthority.ca/news/releases/Pages/2021/May/COVID-19-Immunization-update-effective-May-6.aspx'},
+        {'expect': True, 'tweet': 'COVID-19 immunization age eligibility for May 5 remains at 37+ provincially & 18+ for those living in the NSAD. Reminder to residents that clinic availability is based on vaccine availability, and demand for appointments outweighs the current supply. More: https://saskhealthauthority.ca/news/releases/Pages/2021/May/COVID-19-immunization-alert-effective-May-5.aspx'},
+        {'expect': True, 'tweet': '⚠️Effective 8am May 4, 2021 eligibility for immunization across SK moves to age 37+ except for the Northern Saskatchewan Administration District where it moves to 18+. This applies to all clinics: booked appts, drive-thru/walk-in, mobile & pharmacy. \n\nhttps://saskhealthauthority.ca/news/releases/Pages/2021/May/COVID-19-Immunization-Alert-Effective-May-4.aspx'},
+        {'expect': False, 'tweet': 'Starting TOMORROW, Saturday, May 1 @Prairieland COVID-19 vaccine drive-thru (entry off St. Henry Ave) from 8:30 a.m. to 8:00 p.m. for anyone who meets current vaccine eligibility. The drive-thru will be open through the weekend and into next week as vaccine supplies allow.'},
+    ]
+
+    for i in inputs:
+        result = main.should_retweet(i['tweet'])
+        assert result == i['expect'], f'should_retweet() failed, {i["tweet"]} should have been {i["expect"]}'
+
+
 if __name__ == '__main__':
     print('Running Tests')
     test_get_html()
     test_get_current_booking()
     test_compose_tweet()
     test_should_tweet()
+    test_should_retweet()
     print('All Tests Passed')
